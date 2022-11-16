2045da2b744b7413284b87494773d034e4562c89
Pho <phonum5@gmail.com>
2022-11-16 23:35:25 +0000

    major ui tweaks

--- components/Currency.jsx
+++ components/Currency.jsx
@@ -1,21 +1,63 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { AntDesign, Ionicons, FontAwesome, Entypo } from "@expo/vector-icons";
 
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { AntDesign,Ionicons } from '@expo/vector-icons';
-
-function Currency() {
+const Send = () => {
   return (
-    <View style={styles.card}>
-      <TouchableOpacity style={styles.currencySelection}>
-      <Ionicons name="person-circle-outline" size={24} color="black" />
-        <Text style={styles.currencyName}>GMD</Text>
-        <AntDesign name="caretdown" size={20} color="black" />
-      </TouchableOpacity>
+    <View style={styles.send}>
+      <FontAwesome name="send-o" size={20} color="#ffffde" />
+      <Text style={styles.sendText}>Send USD to GMD</Text>
+      <Text style={styles.sendText}>view quote</Text>
+      <Entypo name="chevron-thin-right" size={18} color="#ffffde" />
     </View>
-  )
+  );
+};
+
+function Currency({ iso, symbol, active }) {
+  return (
+    <View
+      style={
+        active
+          ? styles.container
+          : {
+              marginBottom: 8,
+              borderColor: "#aaaaaa22",
+              borderWidth: 1,
+              borderRadius: 7,
+            }
+      }
+    >
+      <View style={styles.card}>
+        <TouchableOpacity style={styles.currencySelection}>
+          <Ionicons name="person-circle-outline" size={24} color="black" />
+          <Text style={styles.currencyName}>{iso}</Text>
+          {active && (
+            <Entypo name="chevron-thin-down" size={18} color="#242424aa" />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.exchange}>
+          <Text style={styles.amount}>{symbol}0.00</Text>
+          <Text style={styles.rate}>1 USD = 60 GMD</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.keypadButton}>
+          <Ionicons name="ios-calculator-outline" size={27} color="#24242455" />
+        </TouchableOpacity>
+      </View>
+
+      {/* <Send /> */}
+      {active && <Send />}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 2,
+    backgroundColor: "#0466c8ee",
+    marginBottom: 8,
+    borderRadius: 9,
+  },
   button: {
     backgroundColor: "#242424",
     padding: 7,
@@ -32,20 +74,64 @@ const styles = StyleSheet.create({
   },
 
   card: {
-    borderWidth: 1,
-    borderColor: '#aaaaaaaa',
-    borderRadius: 5,
-    padding: 5
+    backgroundColor: "white",
+    borderRadius: 7,
+    flexDirection: "row",
+    paddingLeft: 12,
   },
+
+  notActive: {},
 
   currencySelection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 16,
+    flex: 1,
   },
+
   currencyName: {
-    fontSize: 16
-  }
+    fontSize: 18,
+    color: "#242424",
+    fontWeight: "300",
+    marginHorizontal: 7,
+  },
+
+  exchange: {
+    justifyContent: "center",
+    padding: 5,
+    alignItems: "flex-end",
+    // backgroundColor: 'red'
+  },
+
+  amount: {
+    fontSize: 20,
+  },
+
+  rate: {
+    fontSize: 14,
+    fontWeight: "300",
+    color: "#aaaaaa",
+  },
+
+  keypadButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5,
+    borderLeftWidth: 1,
+    borderLeftColor: "#aaaaaa55",
+  },
+
+  send: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 10,
+  },
+
+  sendText: {
+    color: "#ffffde",
+    fontSize: 15,
+  },
 });
 
-export default Currency
+export default Currency;
--- screens/Converter.jsx
+++ screens/Converter.jsx
@@ -1,20 +1,88 @@
-import React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import React from "react";
+import { StyleSheet, ScrollView, Text, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import Currency from "../components/Currency";
 
-import Currency from '../components/Currency';
+const currencyData = [
+  {
+    iso: "TTD",
+    currency_name: "Trinidadian Dollar",
+    is_obsolete: false,
+    currency_symbol: "TT$",
+    active: true,
+  },
+  {
+    iso: "TVD",
+    currency_name: "Tuvaluan Dollar",
+    is_obsolete: false,
+    currency_symbol: "$",
+    active: false,
+  },
+  {
+    iso: "TWD",
+    currency_name: "Taiwan New Dollar",
+    is_obsolete: false,
+    currency_symbol: "NT$",
+    active: false,
+  },
 
-function Converter()  {
-    return (
-        <ScrollView style={styles.container}>
-            <Currency />
-        </ScrollView>
-    );
+  {
+    iso: "USD",
+    currency_name: "US Dollar",
+    is_obsolete: false,
+    currency_symbol: "$",
+    active: false,
+  },
+  {
+    iso: "UYU",
+    currency_name: "Uruguayan Peso",
+    is_obsolete: false,
+    currency_symbol: "$U",
+    active: false,
+  },
+];
+
+function Converter() {
+  const [currencies, setCurrencies] = React.useState(currencyData);
+
+  return (
+    <ScrollView style={styles.container}>
+      {currencies.map((currency) => (
+        <Currency
+          key={currency.iso}
+          active={currency.active}
+          iso={currency.iso}
+          symbol={currency.currency_symbol}
+        />
+      ))}
+      <TouchableOpacity style={styles.addCurrency}>
+        <AntDesign name="plus" size={16} color="#059BFF" />
+        <Text style={styles.text}>Add Currency</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 5
-    }
-})
+  container: {
+    padding: 15,
+  },
+
+  addCurrency: {
+    backgroundColor: "#42ABF112",
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 25,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    color: "#059BFF",
+    fontSize: 17,
+    marginLeft: 5,
+  },
+});
 
 export default Converter;
