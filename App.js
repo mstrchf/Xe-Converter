4a87cda5e27c74a5798a566a0215e1d66cce7a54
Pho <phonum5@gmail.com>
2022-11-16 14:40:43 +0000

    minor modifications

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+const logo = require('./assets/xe.png') 
+import Converter from "./screens/Converter";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={styles.mainText}>Home Screen</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Main")}
+      >
+        <Text style={styles.buttonText}>Go to Main</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
+function MainScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={styles.mainText}>Main Screen</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Home")}
+      >
+        <Text style={styles.buttonText}>Go to Home</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function Loader() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+    <Image source={logo}/>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 5000);
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Main"
+          component={Converter}
+          options={{
+            title: "Xe Currency",
+            headerTitleStyle: {
+              fontSize: 16,
+              color: "#242424",
+              fontWeight: "400",
+            },
+          }}
+        />
+      </Stack.Navigator>
+
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#242424",
+    padding: 7,
+    borderRadius: 5,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+
+  mainText: {
+    fontSize: 24,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
